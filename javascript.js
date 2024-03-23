69711174b35655e1b6571e70592a4880e774727d
Jean-Marie Schlömer <chengmingjms@gmail.com>
2024-03-23 19:47:25 +0700

    Added playing functionality with prompt

--- javascript.js
+++ javascript.js
@@ -10,4 +10,26 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+
+function playRound(userChoice, computerChoice) {
+    if ((userChoice == "rock" & computerChoice == "paper") | (userChoice == "paper" & computerChoice == "scissors") | (userChoice == "scissors" & computerChoice == "rock")) {
+        return "You lose. " + computerChoice.charAt(0).toUpperCase() + computerChoice.substr(1) + " beats " + userChoice.charAt(0).toUpperCase() + userChoice.substr(1) + ".";
+    }
+    if ((userChoice == "paper" & computerChoice == "rock") | (userChoice == "scissors" & computerChoice == "paper") | (userChoice == "rock" & computerChoice == "scissors")) {
+        return "You win. " + userChoice.charAt(0).toUpperCase() + userChoice.substr(1) + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.substr(1) + ".";
+    }
+    if (userChoice == computerChoice) {
+        return "Draw. You both chose " + userChoice.charAt(0).toUpperCase() + userChoice.substr(1) + ".";
+    }
+}
+
+
+function playGame() {
+    for (var i = 0; i < 5; i++) {
+        const userChoice = prompt('Choose "Rock", "Paper", or "Scissors"');
+        const computerChoice = getComputerChoice();
+        console.log(playRound(userChoice.toLowerCase(), computerChoice.toLowerCase()))
+    }
+}
+
+playGame()
