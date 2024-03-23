274317111db36d061f4890f1dfc42d34beba1ea7
Jean-Marie Schlömer <chengmingjms@gmail.com>
2024-03-23 19:29:40 +0700

    Added getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-function getComputerChoice {
-    
+function getComputerChoice() {
+    choice = Math.floor(3 * Math.random());
+    switch(choice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
 }
+
+console.log(getComputerChoice())
